f5ba357a62e6e235e74bf0e2b95983113b23263e
t0m3kz <tomek@mastuazi.com>
2026-05-21 15:22:28 +0200

    updated tests

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,10 +1,7 @@
 """Unit tests for fscanner.scanner."""
 
 import datetime
-import os
-import tempfile
 import time
-from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 import pytest
